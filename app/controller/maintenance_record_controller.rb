c2ae5e01b69bad08500c256fc19c3dec2905bd8a
haki9975 <elias.robert.hakim@gmail.com>
2021-03-24 14:33:56 -0400

    Maintenence Record Controller Almost Sorted

--- app/controller/maintenance_record_controller.rb
+++ app/controller/maintenance_record_controller.rb
@@ -1,44 +1,47 @@
 class MaintenanceRecordController < ApplicationController
     #Read all records ***this works!***
-        get '/maintenance_records/' do
+        get '/maintenance_records' do
             @mrecords = MaintenanceRecord.all
             erb :'maintenance_records/index'
         end
 
-    #Create new record (render form) ***This works!*** Add edit button and more paramaters
-        get '/maintenance_records/new/' do
+    #Create new record (render form) ***This works!*** 
+        get '/maintenance_records/new' do
             erb :'maintenance_records/new'
         end
 
     #Read one record ***This works!***
-        get '/maintenance_records/:id/' do
+        get '/maintenance_records/:id' do
             @mrecords = MaintenanceRecord.find_by_id(params[:id])
-            erb :'maintenance_records/show'
+              erb :'maintenance_records/show'
         end
 
     #Create new record (save in db) ***This seems to be working!***
-        post '/maintenance_records/' do
-           mrecords = MaintenanceRecord.new(name: params["name"])
+        post '/maintenance_records' do
+           mrecords = MaintenanceRecord.new(params[:mrecords])
            if mrecords.save 
                 redirect "/maintenance_records/#{mrecords.id}"
            else
-            "Error, didn't save" # can "redirect "/movies/new" <--- redirects to path
+                redirect "/maintenance_records/new" 
            end
         end
 
-    #Update one record (render form) ***This is not working***
+    #Update one record (render form) ***This is working!!!***
         get '/maintenance_records/:id/edit' do
             @mrecords = MaintenanceRecord.find_by_id(params[:id])
-            erb :'maintenance_records/edit'
+             erb :'maintenance_records/edit'
         end
-    #Update one record (save in db)
-        patch '/maintenance_records/:id/' do
+
+
+    #Update one record (save in db) ***This is working!!!***
+        patch '/maintenance_records/:id' do
             mrecords = MaintenanceRecord.find_by_id(params[:id])
-            mrecords.name = params["title"]
-            if mrecord.save
-                redirect '/maintenance_records/"#{mrecord.id}"'
+            #binding.pry
+            mrecords.update_attributes(params[:mrecords])
+            if mrecords.save
+                redirect "/maintenance_records/#{mrecords.id}"
             else
-                redirect 'maintenance_records/:id/edit'
+                redirect "maintenance_records/:id/edit"
             end
 
         end
@@ -46,9 +49,13 @@ class MaintenanceRecordController < ApplicationController
     #Delete one movie ***Not working yet***
         delete 'maintenance_records/:id' do
            mrecords = MaintenanceRecord.find_by_id(params[:id])
-           mrecords.destroy
-
-           redirect "/maintenance_records/"
+           binding.pry
+           mrecords.destroy(mrecords.id)
+            if mrecords.save
+              redirect "/maintenance_records"
+            else
+              redirect "/maintenance_records/#{mrecords.id}"
+            end
         end
 
 end
